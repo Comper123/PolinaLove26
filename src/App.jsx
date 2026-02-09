8f3484e812447919ef5b2e3802e4289afb13e9eb
Ilia <ibarishnikov07@mail.ru>
2026-02-09 23:57:14 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  
+
+  return (
+    <div>
+      <p className='text-red-600'>Привет</p>
+    </div>
+  )
+}
